39ee39adbd77686a8bff9f012245745b0c3518fe
Sebastián Pineda <spineda0424@gmail.com>
2024-09-22 23:42:04 -0500

    feat: deserializar respuesta JSON de la API

--- src/main/java/com/weatherdashboard/api/WeatherApiClient.java
+++ src/main/java/com/weatherdashboard/api/WeatherApiClient.java
@@ -1,9 +1,17 @@
 package com.weatherdashboard.api;
 
+import com.google.gson.Gson;
 import com.weatherdashboard.config.AppConfig;
+import com.weatherdashboard.model.response.WeatherResponse;
+import com.weatherdashboard.util.HttpUtils;
 
 public class WeatherApiClient {
-    public void getCurrentWeather(String city) {
+    public static WeatherResponse getCurrentWeather(String city) {
         final String URI_STRING = AppConfig.getBaseUrl() + "?key=" + AppConfig.getApiKey() + "&q=" + city;
+        String apiResponse = HttpUtils.getCurrentWeather(URI_STRING);
+        Gson gson = new Gson();
+        WeatherResponse weatherResponse = gson.fromJson(apiResponse, WeatherResponse.class);
+
+        return weatherResponse;
     }
 }
